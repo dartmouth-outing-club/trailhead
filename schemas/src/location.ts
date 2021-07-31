4a5728fe39f905b8698690e243e43b63a67dfdbc
Ziray Hao <ziray.hao@nightwatchapp.com>
2021-07-31 09:44:32 -0400

    add KnownLocations enum

---
+++ schemas/src/location.ts
@@ -0,0 +1,14 @@
+export enum KnownLocations {
+  // core stuff
+  LODGE = 'Lodge',
+  // hiking
+  SMARTS = 'Smarts',
+  MOOSILAUKE = 'Moosilauke',
+  ASCUTNEY = 'Mount Ascutney', // say 'Mount' if it's not a popular or well-known name
+  CARDIGAN = 'Cardigan',
+  VELVET_ROCKS = 'Velvet Rocks',
+  GILE = 'Gile',
+  HOLTS_LEDGE = "Holt's Ledge",
+  // paddling
+  HARTLANDS = 'Hartlands',
+}
