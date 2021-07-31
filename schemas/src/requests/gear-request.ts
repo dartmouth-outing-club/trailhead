db03e4f0a3129171fc82e45a0b2eae216898e3f3
Ziray Hao <ziray.hao@nightwatchapp.com>
2021-07-31 09:43:02 -0400

    fix wrong gear type

--- schemas/src/requests/gear-request.ts
+++ schemas/src/requests/gear-request.ts
@@ -2,9 +2,9 @@ import { GroupGear, IndividualGear } from 'schemas/src/gear'
 import { Request } from 'schemas/src/requests/request'
 
 export type GroupGearRequest = {
-  gears: Record<string, GroupGear[]>
+  gears: Record<string, GroupGear>
 } & Request
 
 export type IndividualGearRequest = {
-  gears: Record<string, IndividualGear[]>
+  gears: Record<string, IndividualGear>
 } & Request
